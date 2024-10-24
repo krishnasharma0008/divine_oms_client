1d8c15f3982a1849b2ad71ab885a6d6df349e056
krishnasharma0008 <krishna.sharma@hotmail.com>
2024-10-24 19:09:59 +0530

    #

--- src/wrapper/auth-layout.tsx
+++ src/wrapper/auth-layout.tsx
@@ -1,24 +1,25 @@
 // app/auth-layout.tsx
-import React, { useContext, ReactNode, useEffect } from "react";
-import LoginContext from "@/context/login-context";
+import React, { ReactNode, useEffect } from "react";
+//import LoginContext from "@/context/login-context";
 import { useRouter, usePathname } from "next/navigation";
+import { getToken } from "@/local-storage";
 
 const AuthLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const { isLogin } = useContext(LoginContext);
+  //const { isLogin } = useContext(LoginContext);
   const router = useRouter();
   const pathname = usePathname();
 
   useEffect(() => {
     //console.log("Auth!isLogin", isLogin);
-    if (!isLogin && pathname !== "/login") {
-      //console.log("Redirecting to /login from Auth 1");
+    if (!getToken() && pathname !== "/login") {
+      console.log("Redirecting to /login from Auth 1");
       router.push("/login");
-    } 
-  }, [isLogin, pathname, router]);
+    }
+  }, [pathname, router]);
 
   // Display a loading message while checking the login status
-  if (!isLogin && pathname !== "/login") {
-    //console.log("Redirecting to /login from Auth 2");
+  if (!getToken() && pathname !== "/login") {
+    console.log("Redirecting to /login from Auth 2");
     return <p>Loading...</p>;
   } else {
     return <>{children}</>; // Render children if logged in or on the login page
