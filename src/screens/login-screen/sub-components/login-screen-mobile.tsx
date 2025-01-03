8840e4f4d0d5d1a323783afe5ee9eb44f7b66379
krishnasharma0008 <krishna.sharma@hotmail.com>
2025-01-03 16:43:47 +0530

    excel import data update

--- src/screens/login-screen/sub-components/login-screen-mobile.tsx
+++ src/screens/login-screen/sub-components/login-screen-mobile.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { NOTIFICATION_MESSAGES } from "@/config";
 import LoaderContext from "@/context/loader-context";
 import NotificationContext from "@/context/notification-context";
@@ -10,163 +10,107 @@ import LoginContext from "@/context/login-context";
 
 const LoginScreenMobileInput: React.FC = () => {
   const router = useRouter();
-
-  const { setEmailOrMobile, verifyOtp, isOtpVerified } =
-    useContext(LoginContext);
+  const { setEmailOrMobile, verifyOtp } = useContext(LoginContext);
   const { showLoader, hideLoader } = useContext(LoaderContext);
   const { notifyErr, notify } = useContext(NotificationContext);
 
   const [loginType, setLoginType] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-  const [loginTypeError, setLoginTypeError] = useState(false); // To track error for loginType
-  const [passwordError, setPasswordError] = useState(false); // To track error for password
-
-  useEffect(() => {
-    console.log("Updated isOtpVerified:", isOtpVerified);
-  }, [isOtpVerified]);
-
-  // const Resend = async () => {
-  //   try {
-  //     showLoader();
-  //     setIsLoading(true);
-  //     //await loginGetOTP(mobileNumber);
-  //     //window.location.reload();
-  //   } catch (err) {
-  //     console.log("LoginScreenOtp", err);
-  //     notifyErr(NOTIFICATION_MESSAGES.SOMETHING_WRONG);
-  //     setIsLoading(false);
-  //     hideLoader();
-  //   }
-  // };
+  const [loginTypeError, setLoginTypeError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const onClickHandler = async () => {
     showLoader();
-    setIsLoading(true); // Set loading state
+    setIsLoading(true);
     try {
-      // const isValidEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
-      //   loginType
-      // );
-      // const isValidPhoneNumber = /^\d{10}$/.test(loginType);
-
-      // if (isValidEmail || isValidPhoneNumber) {
-      // if (password.trim().length < 6) {
-      //   notifyErr("Password must be at least 6 characters long");
-      //   return;
-      // }
-      //console.log(loginType);
-
-      // Check if the email/phone number or password is empty
       if (!loginType.trim()) {
-        setLoginTypeError(true); // Mark loginType as error
+        setLoginTypeError(true);
         notifyErr("Username cannot be empty");
         setIsLoading(false);
         hideLoader();
         return;
       } else {
-        setLoginTypeError(false); // Reset error state for loginType
+        setLoginTypeError(false);
       }
 
       if (!password.trim()) {
-        setPasswordError(true); // Mark password as error
+        setPasswordError(true);
         notifyErr("Password cannot be empty");
         setIsLoading(false);
         hideLoader();
         return;
       } else {
-        setPasswordError(false); // Reset error state for password
+        setPasswordError(false);
       }
 
       setEmailOrMobile(loginType);
-      //console.log("2");
-      const isValid = await verifyOtp(loginType, password); // Pass only the OTP
+      const isValid = await verifyOtp(loginType, password);
       if (isValid) {
         notify(NOTIFICATION_MESSAGES.LOGIN_SUCCESS);
-        router.push("/"); // Redirect after successful OTP verification
+        router.push("/");
       } else {
         notifyErr(NOTIFICATION_MESSAGES.LOGIN_FAIL);
       }
-      console.log("Navigating to dashboard...");
-      //router.push("/");
-      // } else {
-      //   notifyErr("Invalid Email or Mobile Number");
-      // }
     } catch (err) {
-      console.error("LoginScreenMobileInput - Error during login:", err);
+      console.error("Error during login:", err);
       notifyErr(NOTIFICATION_MESSAGES.SOMETHING_WRONG);
     } finally {
-      setIsLoading(false); // Reset loading state
+      setIsLoading(false);
       hideLoader();
     }
   };
 
   return (
-    <div className="flex flex-col min-h-screen justify-between">
-      {/* Header Section */}
-      <header className="w-full p-4 text-black text-left  text-3xl">
-        Login
-      </header>
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
 
-      <div className="flex-grow w-full flex flex-col items-left px-4">
-        <div className="w-full flex flex-col items-left mb-8">
-          <p className="text-sm text-[#525252] text-left">
-            Divine Solitaires Order Management System
-          </p>
-        </div>
+      {/* Main */}
+      <main className="flex-grow flex items-center justify-center px-4">
         <div className="w-full max-w-sm">
+          <header className="bg-white w-full text-black text-left text-3xl">
+            Login
+          </header>
+          <div className="mt-4 mb-8 text-left">
+            <p className="text-sm text-[#525252]">
+              Divine Solitaires Order Management System
+            </p>
+          </div>
           <label
             htmlFor="loginType"
             className="text-gray-800 text-sm mb-2 block"
           >
             Email/Mobile Number
           </label>
-          <div className="relative flex items-center mb-2">
-            <input
-              id="loginType"
-              type="text"
-              placeholder="Email/Mobile Number"
-              className={`w-full text-gray-800 text-sm border px-4 py-3 rounded-md outline-blue-600 ${
-                loginTypeError ? "border-red-500" : "border-gray-300"
-              }`}
-              value={loginType}
-              onChange={(e) => setLoginType(e.target.value)}
-              disabled={isLoading} // Disable input when loading
-            />
-          </div>
-        </div>
-        <div className="w-full max-w-sm mt-2">
+          <input
+            id="loginType"
+            type="text"
+            placeholder="Email/Mobile Number"
+            className={`w-full text-gray-800 text-sm border px-4 py-3 rounded-md outline-blue-600 ${
+              loginTypeError ? "border-red-500" : "border-gray-300"
+            }`}
+            value={loginType}
+            onChange={(e) => setLoginType(e.target.value)}
+            disabled={isLoading}
+          />
           <input
             id="password"
             type="password"
-            placeholder="password"
-            className={`w-full text-gray-800 text-sm border px-4 py-3 rounded-md outline-blue-600 ${
+            placeholder="Password"
+            className={`w-full text-gray-800 text-sm border px-4 py-3 rounded-md mt-4 outline-blue-600 ${
               passwordError ? "border-red-500" : "border-gray-300"
             }`}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            disabled={isLoading} // Disable input when loading
+            disabled={isLoading}
           />
-          {/* <div className="text-right mb-4">
-            <button
-              type="button"
-              className="text-blue-600 text-sm underline focus:outline-none"
-              onClick={Resend}
-              disabled={isLoading} // Disable resend while loading
-              aria-label="Resend OTP"
-            >
-              Resend OTP
-            </button>
-          </div> */}
-        </div>
-        <div className="w-full max-w-sm mt-6">
           <button
             type="button"
-            className={`w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-black focus:outline-none ${
+            className={`mt-12 w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-black mt-6 focus:outline-none ${
               isLoading ? "opacity-50 cursor-not-allowed" : ""
             }`}
             onClick={onClickHandler}
-            disabled={isLoading} // Disable button when loading
+            disabled={isLoading}
           >
             {isLoading ? "Loading..." : "Log In"}
           </button>
@@ -176,27 +120,24 @@ const LoginScreenMobileInput: React.FC = () => {
             height={520}
             width={612}
             sizes="100vw"
-            className="w-full my-2"
+            className="w-full my-4"
             priority
           />
+          <div className="w-full text-base bg-[#F3F9FA] font-normal leading-4 tracking-normal text-center p-3 rounded-md">
+            <p className="text-gray-500 whitespace-nowrap">
+              Not Registered Yet? Please Contact{" "}
+              <strong className="text-blue-700">+91 9518373847</strong>
+            </p>
+          </div>
         </div>
-        <div className="text-base bg-[#F3F9FA] font-normal leading-4 tracking-normal text-center p-3 rounded-md">
-          <p className="text-gray-500">
-            Not Registered Yet? Please Contact{" "}
-            <strong className="pinter text-blue-700">+91 9518373847</strong>
-          </p>
-        </div>
-      </div>
-      <div className="mt-4 text-center text-sm">
-        <p>If you have any queries, please email us at.</p>
-        <p className="text-blue-700">
-          {/* customerservice@divinesolitaires.com /  */}
-          helpdesk@divinesolitaires.com
-        </p>
-      </div>
-      <div className="mt-4 text-center text-sm">
-        <p>© 2024 ALL RIGHTS RESERVED</p>
-      </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="w-full text-center text-sm">
+        <p>If you have any queries, please email us at</p>
+        <p className="text-blue-700">helpdesk@divinesolitaires.com</p>
+        <p className="mt-2">© 2024 ALL RIGHTS RESERVED</p>
+      </footer>
     </div>
   );
 };
